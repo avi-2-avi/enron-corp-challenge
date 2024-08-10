475e71a74a9143ef70154c925e9b4159dfc6a159
Cristina <avicon95@protonmail.com>
2024-08-10 13:44:01 -0500

    chore: add initial store to manage email retrieval

---
+++ frontend/src/stores/emailStore.ts
@@ -0,0 +1,29 @@
+import { defineStore } from "pinia";
+import { Email } from "../types/email";
+import { getEmails, getEmail } from "../services/emailService";
+
+interface State {
+  emails: Email[];
+  selectedEmail: Email | null;
+  totalPages: number;
+}
+
+export const useEmailStore = defineStore("email", {
+  state: (): State => {
+    return {
+      selectedEmail: null as Email | null,
+      emails: [] as Email[],
+      totalPages: 0,
+    };
+  },
+  actions: {
+    async fetchEmail(id: string) {
+      this.selectedEmail = await getEmail(id);
+    },
+    async fetchEmails(params = {}) {
+      const response = await getEmails(params);
+      this.emails = response.emails || [];
+      this.totalPages = response.total_pages || 0;
+    },
+  },
+});
