c95e99599b341e1a520883a98bf3de80ffa84cc3
Cristina <avicon95@protonmail.com>
2024-08-10 13:47:22 -0500

    chore: modification of CORS middleware for Chi to accept Vue front requests

--- backend/cmd/backend/main.go
+++ backend/cmd/backend/main.go
@@ -6,9 +6,11 @@ import (
 	"backend/internal/routes"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func main() {
@@ -16,7 +18,14 @@ func main() {
 	database.InitZincSearch()
 
 	r := chi.NewRouter()
-
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{os.Getenv("FRONTEND_BASE_URL")},
+		AllowedMethods:   []string{"GET"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
